8c1c46c7385fb66a5feb62139cb5239cc0781583
Wilfred Emonts <wilfredemonts@gmail.com>
2022-03-15 04:55:41 -0400

    setup RSpec for Jungle app testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
-  get '/about', to: 'about#index'
+  
+  get "about", to: "about#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -17,8 +17,15 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show, :destroy]  
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
